4e5c116a9cb6ee1dbe61e2c40ce62e29f2522149
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2020-06-27 13:25:19 +0100

    let static files to be served by nginx

--- app.js
+++ app.js
@@ -10,12 +10,15 @@ const io = require('socket.io').listen(server);
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
-app.get('/service-worker.js', function(req, res) {
+/*app.get('/service-worker.js', function(req, res) {
 	res.sendFile(path.join(__dirname + '/service-worker.js'));
-});
+});*/
+// serve static files using nginx
 
 // Static Routing
-app.use('/static', express.static(path.join(__dirname, 'public')));
+/*app.use('/static', express.static(path.join(__dirname, 'public')));*/
+// serve static files using nginx
+
 
 // Start the Express server
 server.listen(config.Port, () => console.log('Server running on port '+config.Port));
