68dc856e2b7b6a1ee83fa7db10cab035e6dba727
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2020-06-27 14:07:55 +0100

    routing enhancement

--- app.js
+++ app.js
@@ -10,13 +10,22 @@ const io = require('socket.io').listen(server);
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
-/*app.get('/service-worker.js', function(req, res) {
-	res.sendFile(path.join(__dirname + '/service-worker.js'));
-});*/
-// serve static files using nginx
 
 // Static Routing
 /*app.use('/static', express.static(path.join(__dirname, 'public')));*/
+// serve static files using nginx
+
+
+// Others
+/*app.get('/service-worker.js', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/browser/service-worker.js'));
+});
+app.get('/sitemap.xml', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/seo/sitemap.xml'));
+});
+app.get('/robots.txt', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/seo/robots.txt'));
+});*/
 // serve static files using nginx
 
 
